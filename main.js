2db9c103dd42ab13d5409580b1743a3d67fe698f
Silas Burger <silas.a.burger@gmail.com>
2018-08-19 11:06:35 -0700

    Merged readme from remote

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ var appendMeme = function() {
   var upperText = document.getElementById('upperText').value;
   var lowerText = document.getElementById('lowerText').value;
   var count = 0;
+
+  document.getElementByClassName('meme')[count].style.background-image = img;
+  document.getElementByClassName('meme')[count].style.background-repeat = "no-repeat";
+  document.getElementByClassName('meme')[count].innerHTML = "<p> + " + upperText + "</p>";
+  document.getElementByClassName('meme')[count].innerHTML = "<p>" + lowerText + "</p>";
+  console.log('function run!')
+  count++
 };
