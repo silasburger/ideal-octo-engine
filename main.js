7508eacc0659736840249c8d7e8bd1210b21f035
Silas Burger <silas.a.burger@gmail.com>
2018-08-19 10:03:17 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var appendMeme = function() {
+  var img = document.getElementById('memeURL').value;
+  var upperText = document.getElementById('upperText').value;
+  var lowerText = document.getElementById('lowerText').value;
+  var count = 0;
+};
