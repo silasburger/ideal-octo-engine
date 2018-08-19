ef32094be060b5351e8ec9017789d539ae7cdfee
Silas Burger <silas.a.burger@gmail.com>
2018-08-19 12:46:31 -0700

    updating

--- main.js
+++ main.js
@@ -4,11 +4,24 @@ var appendMeme = function() {
   var lowerText = document.getElementById('lowerText').value;
   var count = 0;
 
-  document.getElementsByClassName('meme')[count].style.backgroundImage = img;
-  document.getElementsByClassName('meme')[count].style.backgroundRepeat =
-    'no-repeat';
-  document.getElementsByClassName('meme')[count].innerHTML =
-    '<p>' + upperText + '</p>' + '<p>' + lowerText + '</p>';
-  console.log('function run!');
-  count++;
+  return function() {
+    document.getElementsByClassName('meme')[count].style.backgroundImage =
+      'URL(' + img + ')';
+    document.getElementsByClassName('meme')[count].style.backgroundRepeat =
+      'no-repeat';
+    document.getElementsByClassName('meme')[count].innerHTML =
+      '<p id="top">' +
+      upperText +
+      '</p>' +
+      '<p id="bottom">' +
+      lowerText +
+      '</p>';
+    console.log('function run!');
+    count++;
+  };
 };
+
+document.getElementById('addMeme').addEventListener('click', function() {
+  var newMeme = appendMeme();
+  newMeme();
+});
