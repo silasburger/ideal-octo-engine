30096678918053d6dc2f40aaabb5ff2d2164fad5
Silas Burger <silas.a.burger@gmail.com>
2018-08-19 18:50:49 -0700

    updating; ;

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 var appendMeme = function() {
-  var img = document.getElementById('memeURL').value;
-  var upperText = document.getElementById('upperText').value;
-  var lowerText = document.getElementById('lowerText').value;
-  var count = 0;
-
+  var count = 1;
   return function() {
-    document.getElementsByClassName('meme')[count].style.backgroundImage =
+    var img = document.getElementById('memeURL').value;
+    var upperText = document.getElementById('upperText').value;
+    var lowerText = document.getElementById('lowerText').value;
+    var container = document.getElementById('container');
+
+    container.appendChild(document.createElement('div'));
+
+    container.getElementsByTagName('div')[count].style.backgroundImage =
       'URL(' + img + ')';
-    document.getElementsByClassName('meme')[count].style.backgroundRepeat =
+    container.getElementsByTagName('div')[count].style.backgroundRepeat =
       'no-repeat';
-    document.getElementsByClassName('meme')[count].innerHTML =
-      '<p id="top">' +
+    container.getElementsByTagName('div')[count].style.backgroundSize =
+      '100% 100%';
+    container.getElementsByTagName('div')[count].innerHTML =
+      '<p class="top">' +
       upperText +
       '</p>' +
-      '<p id="bottom">' +
+      '<p class="bottom">' +
       lowerText +
-      '</p>';
-    console.log('function run!');
-    count++;
+      '</p>' +
+      '<span class="delete"><span>';
+    ++count;
   };
 };
 
+var newMeme = appendMeme();
+
 document.getElementById('addMeme').addEventListener('click', function() {
-  var newMeme = appendMeme();
   newMeme();
+  document.getElementById('lowerText').value = '';
+  document.getElementById('upperText').value = '';
+  document.getElementById('memeURL').value = '';
 });
+
+document
+  .getElementsByClassName('delete')
+  .addEventListener('click', function(e) {
+    e.target.remove();
+    alert('clicked');
+    console.log('clicked');
+  });
